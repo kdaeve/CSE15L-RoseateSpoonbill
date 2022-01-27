73fa5cff0deba6508a3e18ec2217fb917eb29adb
DeltaXxx <triple_h@outlook.com>
2022-01-27 14:16:49 -0800

    FIXED BUG

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -30,10 +30,13 @@ public class MarkdownParse {
                  nextOpenBracket == -1  ) {
                      break;
                  }
-           if(markdown.indexOf("!") != nextOpenBracket - 1){
+                 if(nextCloseBracket + 1 != openParen) {
+                     break;
+                 }
+            if(markdown.indexOf("!") != nextOpenBracket - 1){
                toReturn.add(markdown.substring(openParen + 1, closeParen));
-           }
-
+            }
+           
             currentIndex = closeParen + 1;
         }
         return toReturn;
